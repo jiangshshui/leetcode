369ea564fdbb6dcf64928d7c88b027f9ee8b4b75
jiangshshui <30767399+jiangshshui@users.noreply.github.com>
2017-11-08 16:12:13 +0800

    GrayCode

---
+++ secondPage/GrayCode_89.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        results=[0]
+        for i in range(n):
+            results+=[x+pow(2,i) for x in reversed(results)]
+        return results
+
+s=Solution()
+print(s.grayCode(3))
