1773936e35ca4f53f9d761f60df56acc7c7801e3
jiangshshui <30767399+jiangshshui@users.noreply.github.com>
2018-10-19 23:54:35 +0800

    393

--- eighthPage/393_UTF-8Validation.py
+++ eighthPage/393_UTF-8Validation.py
@@ -1,48 +1,69 @@
+# class Solution:
+#     def validUtf8(self, data):
+#         """
+#         :type data: List[int]
+#         :rtype: bool
+#         """
+#         # 240-247 128-191
+#         # 224 -239
+#         # 192-223
+#         # 127
+#         i=0
+#         while i<len(data):
+#             if data[i]>=240 and data[i]<=247:
+#                 j=i+1
+#                 while j<=min(i+3,len(data)-1) and data[j]>=128 and data[j]<=191:
+#                     j+=1
+#                 if j-i==4:
+#                     i=j
+#                 else:
+#                     return False
+#
+#             elif data[i]>=224 and data[i]<=239:
+#                 j=i+1
+#                 while j<=min(i+2,len(data)-1) and data[j]>=128 and data[j]<=191:
+#                     j+=1
+#                 if j-i==3:
+#                     i=j
+#                 else:
+#                     return False
+#
+#             elif data[i]>=192 and data[i]<=223:
+#                 j=i+1
+#                 while j<=min(i+1,len(data)-1) and data[j]>=128 and data[j]<=191:
+#                     j+=1
+#                 if j-i==2:
+#                     i=j
+#                 else:
+#                     return False
+#
+#             elif data[i]>=0 and data[i]<=127:
+#                 i+=1
+#             else:
+#                 return False
+#         return True
+
+
 class Solution:
     def validUtf8(self, data):
-        """
-        :type data: List[int]
-        :rtype: bool
-        """
-        # 240-247 128-191
-        # 224 -239
-        # 192-223
-        # 127
-        i=0
-        while i<len(data):
-            if data[i]>=240 and data[i]<=247:
-                j=i+1
-                while j<=min(i+3,len(data)-1) and data[j]>=128 and data[j]<=191:
-                    j+=1
-                if j-i==4:
-                    i=j
-                else:
-                    return False
+        n = 0
 
-            elif data[i]>=224 and data[i]<=239:
-                j=i+1
-                while j<=min(i+2,len(data)-1) and data[j]>=128 and data[j]<=191:
-                    j+=1
-                if j-i==3:
-                    i=j
-                else:
-                    return False
+        for i in reversed(range(len(data))):
+            byte = '{:08b}'.format(data[i])
 
-            elif data[i]>=192 and data[i]<=223:
-                j=i+1
-                while j<=min(i+1,len(data)-1) and data[j]>=128 and data[j]<=191:
-                    j+=1
-                if j-i==2:
-                    i=j
-                else:
+            if byte.startswith('0'):
+                if n > 0:
                     return False
-
-            elif data[i]>=0 and data[i]<=127:
-                i+=1
+            elif byte.startswith('10'):
+                n += 1
+                if n == 4:
+                    return False
+            elif byte.startswith('1' + '1' * n + '0'):
+                n = 0
             else:
                 return False
-        return True
 
+        return n == 0
 
 
 if __name__=="__main__":
